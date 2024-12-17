83d7ee1ca479032b9379c23fb7c40c1508037cb6
gianbdev <giancarlo.silva550@gmail.com>
2024-12-17 18:35:55 -0500

    fix | doFilterInternal void

--- src/main/java/com/saludSystem/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/saludSystem/jwt/JwtAuthenticationFilter.java
@@ -11,7 +11,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -27,7 +26,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        /*
     	final String authorizationHeader = request.getHeader("Authorization");
 
         String username = null;
@@ -48,34 +46,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
-        filterChain.doFilter(request, response);*/
-        
-    	
-    	    final String authorizationHeader = request.getHeader("Authorization");
-
-    	    String username = null;
-    	    String jwt = null;
-
-    	    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
-    	        jwt = authorizationHeader.substring(7);
-    	        username = jwtUtil.extractUsername(jwt);
-    	        System.out.println("JWT Filter: Token detected, username = " + username);
-    	    }
-
-    	    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
-    	        UserDetails userDetails = userService.loadUserByUsername(username);
-
-    	        if (jwtUtil.validateToken(jwt, userDetails)){
-    	            UsernamePasswordAuthenticationToken authToken =
-    	                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-    	            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-    	            SecurityContextHolder.getContext().setAuthentication(authToken);
-    	            System.out.println("JWT Filter: Authentication successful for username = " + username);
-    	        } else {
-    	            System.out.println("JWT Filter: Token validation failed.");
-    	        }
-    	    }
-
-    	    filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 }
