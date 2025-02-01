cdd2f3648d19c5cfda938bbc8f756468bc8f1c4f
gianbdev <giancarlo.silva550@gmail.com>
2025-01-31 19:23:17 -0500

    update | next module v1

---
+++ src/main/java/com/saludSystem/entities/Doctor.java
@@ -0,0 +1,16 @@
+package com.saludSystem.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "medicos")
+public class Doctor {
+
+}
