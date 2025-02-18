f9b16e3d68d420175f70700cf7e5fb207fad1c20
gianbdev <giancarlo.silva550@gmail.com>
2025-02-18 16:38:48 -0500

    solve | securitys configs access and generals module completed. Close #5

--- src/main/java/com/saludSystem/config/swagger/SwaggerConfig.java
+++ src/main/java/com/saludSystem/config/swagger/SwaggerConfig.java
@@ -2,9 +2,6 @@ package com.saludSystem.config.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
