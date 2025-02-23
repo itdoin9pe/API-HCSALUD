449fa597d8429e7da53c38841376c44d25c1ef64
gianbdev <giancarlo.silva550@gmail.com>
2025-02-23 15:09:38 -0500

    update | schemas for obtener dto of tipo documento endpoints

--- src/main/java/com/saludSystem/dtos/configuration/TipoDocumento/TipoDocumentoDTO.java
+++ src/main/java/com/saludSystem/dtos/configuration/TipoDocumento/TipoDocumentoDTO.java
@@ -1,5 +1,6 @@
 package com.saludSystem.dtos.configuration.TipoDocumento;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import java.util.UUID;
 
@@ -12,12 +13,16 @@ public class TipoDocumentoDTO {
 
     private String serie;
 
+    @Schema(description = "Valor Inicio", example = "0")
     private Integer inicio;
 
+    @Schema(description = "Valor Fin", example = "0")
     private Integer fin;
 
+    @Schema(description = "Valor Correlativo", example = "0")
     private Integer correlativoActual;
 
+    @Schema(description = "Estado del tipo de Documento", example = "0" )
     private Integer estado;
 
 }
