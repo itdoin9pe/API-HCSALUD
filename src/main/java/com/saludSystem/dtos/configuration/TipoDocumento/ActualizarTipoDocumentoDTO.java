98fd94966424bf163a2797f1d0669d9ab2920f1b
gianbdev <giancarlo.silva550@gmail.com>
2025-02-23 15:11:12 -0500

    update | schemas for actualizar dto of tipo documento endpoints

--- src/main/java/com/saludSystem/dtos/configuration/TipoDocumento/ActualizarTipoDocumentoDTO.java
+++ src/main/java/com/saludSystem/dtos/configuration/TipoDocumento/ActualizarTipoDocumentoDTO.java
@@ -1,5 +1,6 @@
 package com.saludSystem.dtos.configuration.TipoDocumento;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.UUID;
@@ -17,12 +18,16 @@ public class ActualizarTipoDocumentoDTO {
 
     private String serie;
 
+    @Schema(description = "Valor Inicio", example = "0")
     private Integer inicio;
 
+    @Schema(description = "Valor Fin", example = "0")
     private Integer fin;
 
+    @Schema(description = "Valor Correlativo", example = "0")
     private Integer correlativoActual;
 
+    @Schema(description = "Estado del tipo de Documento", example = "0" )
     private Integer estado;
 
 }
