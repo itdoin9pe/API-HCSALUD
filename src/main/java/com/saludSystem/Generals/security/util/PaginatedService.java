1a52b1f368405fa8216c49554f49fe7512e069f0
gianbdev <giancarlo.silva550@gmail.com>
2025-03-13 18:45:40 -0500

    refresh | jar

--- src/main/java/com/saludSystem/Generals/security/util/PaginatedService.java
+++
@@ -1,6 +0,0 @@
-package com.saludSystem.Generals.security.util;
-
-public interface PaginatedService {
-    long getTotalCount();
-    //<T> List<T> getPagedResults(UUID hospitalId, int page, int rows);
-}
