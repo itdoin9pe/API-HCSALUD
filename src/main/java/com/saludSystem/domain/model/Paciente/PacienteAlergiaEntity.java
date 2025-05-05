d507e866388bfee9b7c0af28ae7abe7309534648
gianbdev <giancarlo.silva550@gmail.com>
2025-05-05 17:57:27 -0500

    update | paciente historial entities

---
+++ src/main/java/com/saludSystem/domain/model/Paciente/PacienteAlergiaEntity.java
@@ -0,0 +1,35 @@
+package com.saludSystem.domain.model.Paciente;
+
+import com.saludSystem.domain.model.BaseEntity;
+import com.saludSystem.domain.model.Catalogo.AlergiaEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "pacientes_alergias")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class PacienteAlergiaEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "paciente_alergia_id", nullable = false)
+    private UUID pacienteAlergias;
+
+    @ManyToOne
+    @JoinColumn(name = "alergia_id", nullable = false)
+    private AlergiaEntity alergiaEntity;
+
+    @Column(name = "paciente_alergia_observacion", nullable = false)
+    private String observacion;
+
+    @Column(name = "paciente_alergia_estado", nullable = false)
+    private Integer estado;
+
+}
