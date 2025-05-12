556ac30290cd71611da591ed68fd965d6337d06e
gianbdev <giancarlo.silva550@gmail.com>
2025-05-12 13:22:13 -0500

    Close #67 | solve | diagnosticos endpoints, tabs and swagger docs.

--- src/main/java/com/saludSystem/infrastructure/adapters/in/controllers/Paciente/DiagnosticoController.java
+++ src/main/java/com/saludSystem/infrastructure/adapters/in/controllers/Paciente/DiagnosticoController.java
@@ -1,16 +1,12 @@
 package com.saludSystem.infrastructure.adapters.in.controllers.Paciente;
 
 import com.saludSystem.application.dtos.Paciente.GET.DiagnosticoDTO;
-import com.saludSystem.application.dtos.Paciente.GET.HistorialClinico.AntecedenteDTO;
 import com.saludSystem.application.dtos.Paciente.POST.CrearDiagnosticoDTO;
-import com.saludSystem.application.dtos.Paciente.POST.HistorialClinico.CrearAntecedenteDTO;
 import com.saludSystem.application.dtos.Paciente.PUT.ActualizarDiagnosticoDTO;
-import com.saludSystem.application.dtos.Paciente.PUT.HistorialClinico.ActualizarAntecedenteDTO;
 import com.saludSystem.application.services.Paciente.DiagnosticoService;
 import com.saludSystem.infrastructure.adapters.in.response.ApiResponse;
 import com.saludSystem.infrastructure.adapters.in.response.ListResponse;
 import com.saludSystem.infrastructure.adapters.in.response.Paciente.DiagnosticoResponse;
-import com.saludSystem.infrastructure.adapters.in.response.Paciente.HistorialClinico.AntecedenteResponse;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
