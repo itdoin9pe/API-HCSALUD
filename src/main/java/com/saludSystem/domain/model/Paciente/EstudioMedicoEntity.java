f9053b3d4ed159c10dfd9d57da28c2517036dc06
gianbdev <giancarlo.silva550@gmail.com>
2025-05-14 19:05:12 -0500

    update | images estudios cli module

---
+++ src/main/java/com/saludSystem/domain/model/Paciente/EstudioMedicoEntity.java
@@ -0,0 +1,50 @@
+package com.saludSystem.domain.model.Paciente;
+
+import com.saludSystem.domain.model.BaseEntity;
+import com.saludSystem.domain.model.Medico.DoctorEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pacientes_estudios_medicos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class EstudioMedicoEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_paciente_estudio", nullable = false)
+    private Long id;
+
+    @Column(name = "tipo_estudio", nullable = false)
+    private String tipo;
+
+    @Column(name = "descripcion_estudio", nullable = false)
+    private String descripcion;
+
+    private LocalDateTime requestAt;
+
+    private LocalDateTime performedAt;
+
+    @Column(name = "estado_estudio", nullable = false)
+    private String estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_paciente")
+    private PacienteEntity pacienteEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "id_doctor")
+    private DoctorEntity doctorEntity;
+
+    @OneToOne(mappedBy = "estudioMedicoEntity", cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_paciente_estudio_resultado")
+    private EstudioResultadoEntity estudioResultadoEntity;
+
+}
