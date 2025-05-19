c595025c8b6668ced75fcdff9d55f708ec651f37
gianbdev <giancarlo.silva550@gmail.com>
2025-05-19 16:17:48 -0500

    update | recetas module

---
+++ src/main/java/com/saludSystem/domain/model/Paciente/MedicamentoRecetadoEntity.java
@@ -0,0 +1,44 @@
+package com.saludSystem.domain.model.Paciente;
+
+import com.saludSystem.domain.model.BaseEntity;
+import com.saludSystem.domain.model.Catalogo.MedicamentoEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "pacientes_medicamentos_recetados")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class MedicamentoRecetadoEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receta_id", nullable = false)
+    private RecetaEntity recetaEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medicamento_id", nullable = false)
+    private MedicamentoEntity medicamentoEntity;
+
+    @Column(nullable = false)
+    private String dosis; // ej: "500 mg"
+
+    @Column(nullable = false)
+    private String frecuencia; // ej: "3 veces al día"
+
+    @Column(nullable = true)
+    private Integer duracionDias; // duración en días
+
+    @Column(length = 500)
+    private String indicaciones; // indicaciones adicionales
+
+}
