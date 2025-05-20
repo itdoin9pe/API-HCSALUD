a2d5a96e734d40def61f96c98d0ac637771d4180
gianbdev <giancarlo.silva550@gmail.com>
2025-05-20 17:58:22 -0500

    add | recetas dtos

---
+++ src/main/java/com/saludSystem/application/services/Paciente/RecetaService.java
@@ -0,0 +1,4 @@
+package com.saludSystem.application.services.Paciente;
+
+public interface RecetaService {
+}
---
+++ src/main/java/com/saludSystem/infrastructure/adapters/out/persistance/repository/Paciente/RecetaRepository.java
@@ -0,0 +1,4 @@
+package com.saludSystem.infrastructure.adapters.out.persistance.repository.Paciente;
+
+public interface RecetaRepository {
+}
