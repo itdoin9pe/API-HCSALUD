f047b28bbb04b0db858d9c094b8ecc3ee5b2c70e
gianbdev <giancarlo.silva550@gmail.com>
2025-06-09 15:29:46 -0500

    Remove .env.prod from version control

--- src/main/java/com/saludSystem/infrastructure/adapters/out/persistance/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.saludSystem.infrastructure.adapters.out.persistance;
-
-import com.saludSystem.domain.model.ServicioEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<ServicioEntity, Long> {
-}
