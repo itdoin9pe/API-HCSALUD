9299830c3ef001449fea8f241b2280158426efcd
gianbdev <giancarlo.silva550@gmail.com>
2025-06-20 13:20:46 -0500

    refactor | tipo citado config - catalog module

--- src/main/java/com/saludSystem/application/services/Catalogo/TipoCitadoService.java
+++ src/main/java/com/saludSystem/application/services/Catalogo/TipoCitadoService.java
@@ -3,24 +3,13 @@ package com.saludSystem.application.services.Catalogo;
 import com.saludSystem.application.dtos.Catalogo.POST.CrearTipoCitadoDTO;
 import com.saludSystem.application.dtos.Catalogo.GET.TipoCitadoDTO;
 import com.saludSystem.application.dtos.Catalogo.PUT.ActualizarTipoCitadoDTO;
-import com.saludSystem.infrastructure.adapters.in.response.ApiResponse;
-import com.saludSystem.infrastructure.adapters.in.response.ListResponse;
+import com.saludSystem.application.services.GenericService;
 
-import java.util.List;
 import java.util.UUID;
 
-public interface TipoCitadoService {
-
-    ApiResponse saveTipoCitado(CrearTipoCitadoDTO crearTipoCitadoDTO);
-
-    ApiResponse updateTipoCitado(UUID tipoCitadoId, ActualizarTipoCitadoDTO actualizarTipoCitadoDTO);
-
-    TipoCitadoDTO getTipoCitadoById(UUID tipoCitadoId);
-
-    List<TipoCitadoDTO> getTipoCitadoList();
-
-    ApiResponse deleteTipoCitado(UUID tipocitadoId);
-
-    ListResponse<TipoCitadoDTO> getAllTipoCitado(UUID hospitalId, int page, int rows);
-
+public interface TipoCitadoService extends GenericService<
+        TipoCitadoDTO,
+        UUID,
+        CrearTipoCitadoDTO,
+        ActualizarTipoCitadoDTO> {
 }
--- src/main/java/com/saludSystem/infrastructure/adapters/in/controllers/Catalogo/TipoCitadoController.java
+++ src/main/java/com/saludSystem/infrastructure/adapters/in/controllers/Catalogo/TipoCitadoController.java
@@ -3,67 +3,36 @@ package com.saludSystem.infrastructure.adapters.in.controllers.Catalogo;
 import com.saludSystem.application.dtos.Catalogo.PUT.ActualizarTipoCitadoDTO;
 import com.saludSystem.application.dtos.Catalogo.POST.CrearTipoCitadoDTO;
 import com.saludSystem.application.dtos.Catalogo.GET.TipoCitadoDTO;
-import com.saludSystem.infrastructure.adapters.in.response.ApiResponse;
+import com.saludSystem.application.services.GenericService;
+import com.saludSystem.infrastructure.adapters.in.controllers.GenericController;
 import com.saludSystem.infrastructure.adapters.in.response.Catalogo.TipoCitadoResponse;
 import com.saludSystem.infrastructure.adapters.in.response.ListResponse;
-import com.saludSystem.application.services.Catalogo.TipoCitadoService;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @Tag(name = "TiposCitado")
 @RestController
 @RequestMapping("/api/TiposCitado")
-public class TipoCitadoController {
+public class TipoCitadoController extends GenericController<
+        TipoCitadoDTO, UUID, CrearTipoCitadoDTO, ActualizarTipoCitadoDTO> {
 
-    private final TipoCitadoService tipoCitadoService;
-
-    public TipoCitadoController(TipoCitadoService tipoCitadoService) {
-        this.tipoCitadoService = tipoCitadoService;
+    protected TipoCitadoController(GenericService<TipoCitadoDTO, UUID,
+            CrearTipoCitadoDTO, ActualizarTipoCitadoDTO> genericService) {
+        super(genericService);
     }
 
-    @PostMapping("/SaveTipoCitado")
-    public ApiResponse store(@Valid @RequestBody CrearTipoCitadoDTO crearTipoCitadoDTO){
-        return tipoCitadoService.saveTipoCitado(crearTipoCitadoDTO);
-    }
-
-    @GetMapping("/GetAllTipoCitado")
+    @GetMapping("/GetAll")
     @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Operación exitosa",
-                    content = @Content(mediaType = "application/json",
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200",
+                    description = "Operación exitosa", content = @Content(mediaType = "application/json",
                             schema = @Schema(implementation = TipoCitadoResponse.class)))
     })
-    public ListResponse<TipoCitadoDTO> getAllPage(
-            @RequestParam(name = "hospitalId", required = true) UUID hospitalId, @RequestParam(name = "Page") int page,
-            @RequestParam(name = "Rows") int rows) {
-        return tipoCitadoService.getAllTipoCitado(hospitalId, page, rows);
+    public ListResponse<TipoCitadoDTO> getAllPaginated(UUID hospitalId, int page, int rows) {
+        return super.getAllPaginated(hospitalId, page, rows);
     }
-
-    @GetMapping("/GetTipoCitadoList")
-    public ResponseEntity<List<TipoCitadoDTO>> getAllList() {
-        return ResponseEntity.ok(tipoCitadoService.getTipoCitadoList());
-    }
-
-    @GetMapping("/GetTipoCitado/{tipoCitadoId}")
-    public TipoCitadoDTO getById(@PathVariable UUID tipoCitadoId) {
-        return tipoCitadoService.getTipoCitadoById(tipoCitadoId);
-    }
-
-    @PutMapping("/UpdateTipoCitado/{tipoCitadoId}")
-    public ApiResponse update(@PathVariable UUID tipoCitadoId, @RequestBody ActualizarTipoCitadoDTO actualizarTipoCitadoDTO) {
-        return tipoCitadoService.updateTipoCitado(tipoCitadoId, actualizarTipoCitadoDTO);
-    }
-
-    @DeleteMapping("/DeleteTipoCitado/{tipoCitadoId}")
-    public ApiResponse destroy(@PathVariable UUID tipoCitadoId) {
-        return tipoCitadoService.deleteTipoCitado(tipoCitadoId);
-    }
-
 }
