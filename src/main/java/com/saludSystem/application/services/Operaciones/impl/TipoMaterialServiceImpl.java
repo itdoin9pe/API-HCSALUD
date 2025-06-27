d56f219b75d5f1d5bbe681721ce17cd69b0ecba0
gianbdev <giancarlo.silva550@gmail.com>
2025-06-27 14:46:37 -0500

    refactor | tipo material configs - operaciones module

--- src/main/java/com/saludSystem/application/services/Operaciones/impl/TipoMaterialServiceImpl.java
+++ src/main/java/com/saludSystem/application/services/Operaciones/impl/TipoMaterialServiceImpl.java
@@ -3,122 +3,75 @@ package com.saludSystem.application.services.Operaciones.impl;
 import com.saludSystem.application.dtos.Operaciones.GET.TipoMaterialDTO;
 import com.saludSystem.application.dtos.Operaciones.POST.CrearTipoMaterialDTO;
 import com.saludSystem.application.dtos.Operaciones.PUT.ActualizarTipoMaterialDTO;
+import com.saludSystem.application.services.GenericServiceImpl;
 import com.saludSystem.application.services.Operaciones.TipoMaterialService;
-import com.saludSystem.domain.exception.ResourceNotFoundException;
-import com.saludSystem.domain.model.Configuracion.SysSaludEntity;
-import com.saludSystem.domain.model.Configuracion.UserEntity;
 import com.saludSystem.domain.model.Operaciones.TipoMaterialEntity;
 import com.saludSystem.infrastructure.adapters.in.response.ApiResponse;
 import com.saludSystem.infrastructure.adapters.in.response.ListResponse;
-import com.saludSystem.infrastructure.adapters.out.persistance.repository.Configuracion.SysSaludRepository;
-import com.saludSystem.infrastructure.adapters.out.persistance.repository.Configuracion.UserRepository;
 import com.saludSystem.infrastructure.adapters.out.persistance.repository.Operaciones.TipoMaterialRepository;
+import com.saludSystem.infrastructure.adapters.out.security.util.AuthValidator;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
-public class TipoMaterialServiceImpl implements TipoMaterialService {
+public class TipoMaterialServiceImpl extends GenericServiceImpl<TipoMaterialEntity, TipoMaterialDTO, UUID,
+        CrearTipoMaterialDTO, ActualizarTipoMaterialDTO> implements TipoMaterialService {
 
-    private final TipoMaterialRepository tipoMaterialRepository;
-    private final SysSaludRepository sysSaludRepository;
-    private final UserRepository userRepository;
-    private final ModelMapper modelMapper;
-
-    public TipoMaterialServiceImpl(TipoMaterialRepository tipoMaterialRepository, SysSaludRepository sysSaludRepository, UserRepository userRepository, ModelMapper modelMapper) {
-        this.tipoMaterialRepository = tipoMaterialRepository;
-        this.sysSaludRepository = sysSaludRepository;
-        this.userRepository = userRepository;
-        this.modelMapper = modelMapper;
+    public TipoMaterialServiceImpl(TipoMaterialRepository tipoMaterialRepository, ModelMapper modelMapper, AuthValidator authValidator) {
+        super(tipoMaterialRepository, modelMapper, authValidator, TipoMaterialDTO.class,
+                tipoMaterialEntity -> modelMapper.map(tipoMaterialEntity, TipoMaterialDTO.class));
     }
 
     @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     @Override
-    public ApiResponse saveTipoMaterial(CrearTipoMaterialDTO crearTipoMaterialDTO) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-        UserEntity userEntity = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-        if (!"ADMINISTRADOR".equals(userEntity.getRol().getNombre())) {
-            return new ApiResponse(false, "No tienes permisos para realizar esta acción");
-        }
-        SysSaludEntity hospital = sysSaludRepository.findById(userEntity.getHospital().getHospitalId())
-                .orElseThrow(() -> new RuntimeException("Hospital no encontrado"));
-        TipoMaterialEntity tipoMaterialEntity = new TipoMaterialEntity();
-        tipoMaterialEntity.setNombre(crearTipoMaterialDTO.getNombre());
-        tipoMaterialEntity.setDescripcion(crearTipoMaterialDTO.getDescripcion());
-        tipoMaterialEntity.setEstado(crearTipoMaterialDTO.getEstado());
-        tipoMaterialEntity.setHospital(hospital);
-        tipoMaterialEntity.setUser(userEntity);
-        tipoMaterialRepository.save(tipoMaterialEntity);
-        return new ApiResponse(true, "Tipo material creado correctamente");
+    public ApiResponse save(CrearTipoMaterialDTO crearTipoMaterialDTO) {
+        return super.save(crearTipoMaterialDTO);
     }
 
     @Override
-    public List<TipoMaterialDTO> getTipoMaterialList() {
-        return tipoMaterialRepository.findAll().stream().map(this::convertToDTO).collect(Collectors.toList());
-    }
-
-    @Override
-    public TipoMaterialDTO getTipoMaterialById(UUID tipoMaterialId) {
-        TipoMaterialEntity tipoMaterialEntity = tipoMaterialRepository.findById(tipoMaterialId)
-                .orElseThrow( () -> new ResourceNotFoundException("Tipo material no encontrado"));
-        return convertToDTO(tipoMaterialEntity);
-    }
-
-    @Override
-    public ListResponse<TipoMaterialDTO> getAllTipoMaterial(UUID hospitalId, int page, int rows) {
-        Pageable pageable = PageRequest.of(page - 1, rows);
-        Page<TipoMaterialEntity> tipoMaterialEntityPage = tipoMaterialRepository.findByHospital_HospitalId(hospitalId, pageable);
-        List<TipoMaterialDTO> data = tipoMaterialEntityPage.getContent().stream().map(this::convertToDTO).collect(Collectors.toList());
-        return new ListResponse<>(data, tipoMaterialEntityPage.getTotalElements(), tipoMaterialEntityPage.getTotalPages(), tipoMaterialEntityPage.getNumber() +1 );
+    public ListResponse<TipoMaterialDTO> getAllPaginated(UUID hospitalId, int page, int rows) {
+        return super.getAllPaginated(hospitalId, page, rows);
     }
 
     @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     @Override
-    public ApiResponse updateTipoMaterial(UUID tipoMaterialId, ActualizarTipoMaterialDTO actualizarTipoMaterialDTO) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-        UserEntity userEntity = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-        if (!"ADMINISTRADOR".equals(userEntity.getRol().getNombre())) {
-            return new ApiResponse(false, "No tienes permisos para realizar esta acción");
-        }
-        TipoMaterialEntity tipoMaterialEntity = tipoMaterialRepository.findById(tipoMaterialId)
-                .orElseThrow( () -> new ResourceNotFoundException("Tipo material no encontrado"));
-        Optional.ofNullable(actualizarTipoMaterialDTO.getNombre()).filter(desc -> !desc.isBlank()).ifPresent(tipoMaterialEntity::setNombre);
-        Optional.ofNullable(actualizarTipoMaterialDTO.getDescripcion()).filter(desc -> !desc.isBlank()).ifPresent(tipoMaterialEntity::setDescripcion);
-        Optional.ofNullable(actualizarTipoMaterialDTO.getEstado()).ifPresent(tipoMaterialEntity::setEstado);
-        tipoMaterialRepository.save(tipoMaterialEntity);
-        return new ApiResponse(true, "Tipo material actualizado correctamente");
+    public ApiResponse update(UUID uuid, ActualizarTipoMaterialDTO actualizarTipoMaterialDTO) {
+        return super.update(uuid, actualizarTipoMaterialDTO);
+    }
+
+    @Override
+    public TipoMaterialDTO getById(UUID uuid) {
+        return super.getById(uuid);
+    }
+
+    @Override
+    public List<TipoMaterialDTO> getList() {
+        return super.getList();
     }
 
     @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     @Override
-    public ApiResponse deleteTipoMaterial(UUID tipoMaterialId) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-        UserEntity userEntity = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-        if (!"ADMINISTRADOR".equals(userEntity.getRol().getNombre())) {
-            return new ApiResponse(false, "No tienes permisos para realizar esta acción");
-        }
-        tipoMaterialRepository.deleteById(tipoMaterialId);
-        return new ApiResponse(true, "Tipo material eliminado correctamente");
+    public ApiResponse delete(UUID uuid) {
+        return super.delete(uuid);
     }
 
-    private TipoMaterialDTO convertToDTO(TipoMaterialEntity tipoMaterialEntity) {
-        return modelMapper.map(tipoMaterialEntity, TipoMaterialDTO.class);
+    @Override
+    protected TipoMaterialEntity convertCreateDtoToEntity(CrearTipoMaterialDTO crearTipoMaterialDTO) {
+        TipoMaterialEntity entity = new TipoMaterialEntity();
+        entity.setNombre(crearTipoMaterialDTO.getNombre());
+        entity.setDescripcion(crearTipoMaterialDTO.getDescripcion());
+        entity.setEstado(crearTipoMaterialDTO.getEstado());
+        return entity;
     }
 
+    @Override
+    protected void updateEntityFromDto(ActualizarTipoMaterialDTO actualizarTipoMaterialDTO, TipoMaterialEntity entity) {
+        entity.setNombre(actualizarTipoMaterialDTO.getNombre());
+        entity.setDescripcion(actualizarTipoMaterialDTO.getDescripcion());
+        entity.setEstado(actualizarTipoMaterialDTO.getEstado());
+    }
 }
--- src/main/java/com/saludSystem/infrastructure/adapters/out/persistance/repository/Operaciones/TipoMaterialRepository.java
+++ src/main/java/com/saludSystem/infrastructure/adapters/out/persistance/repository/Operaciones/TipoMaterialRepository.java
@@ -1,16 +1,10 @@
 package com.saludSystem.infrastructure.adapters.out.persistance.repository.Operaciones;
 
 import com.saludSystem.domain.model.Operaciones.TipoMaterialEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.saludSystem.infrastructure.adapters.out.persistance.repository.GenericRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface TipoMaterialRepository extends JpaRepository<TipoMaterialEntity, UUID> {
-
-    Page<TipoMaterialEntity> findByHospital_HospitalId(UUID hospitalId, Pageable pageable);
+public interface TipoMaterialRepository extends GenericRepository<TipoMaterialEntity> {
 
 }
