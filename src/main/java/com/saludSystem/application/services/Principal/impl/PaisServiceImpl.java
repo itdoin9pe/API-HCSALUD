eb965991063cb2bb4a032bee939e421eea787543
gianbdev <giancarlo.silva550@gmail.com>
2025-06-28 13:47:54 -0500

    refactor | pais config - principal module

--- src/main/java/com/saludSystem/application/services/Principal/impl/PaisServiceImpl.java
+++ src/main/java/com/saludSystem/application/services/Principal/impl/PaisServiceImpl.java
@@ -1,23 +1,22 @@
 package com.saludSystem.application.services.Principal.impl;
 
 import com.saludSystem.application.services.Principal.PaisService;
-import com.saludSystem.domain.model.Configuracion.SysSaludEntity;
-import com.saludSystem.infrastructure.adapters.out.persistance.repository.Configuracion.SysSaludRepository;
-import com.saludSystem.domain.model.Configuracion.UserEntity;
-import com.saludSystem.infrastructure.adapters.out.persistance.repository.Configuracion.UserRepository;
 import com.saludSystem.application.dtos.Principal.POST.CrearPaisDTO;
 import com.saludSystem.application.dtos.Principal.GET.PaisDTO;
+import com.saludSystem.domain.model.Configuracion.SysSaludEntity;
+import com.saludSystem.domain.model.Configuracion.UserEntity;
 import com.saludSystem.domain.model.Principal.PaisEntity;
-import com.saludSystem.infrastructure.adapters.out.persistance.repository.Principal.PaisRepository;
 import com.saludSystem.infrastructure.adapters.in.response.ApiResponse;
 import com.saludSystem.infrastructure.adapters.in.response.ListResponse;
+import com.saludSystem.infrastructure.adapters.out.persistance.repository.Configuracion.SysSaludRepository;
+import com.saludSystem.infrastructure.adapters.out.persistance.repository.Principal.PaisRepository;
+import com.saludSystem.infrastructure.adapters.out.security.util.AuthValidator;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -26,23 +25,20 @@ import java.util.stream.Collectors;
 public class PaisServiceImpl implements PaisService {
 
     private final PaisRepository paisRepository;
-    private final UserRepository userRepository;
     private final SysSaludRepository sysSaludRepository;
+    private final AuthValidator authValidator;
     private final ModelMapper modelMapper;
 
-    public PaisServiceImpl(PaisRepository paisRepository, UserRepository userRepository, SysSaludRepository sysSaludRepository, ModelMapper modelMapper) {
+    public PaisServiceImpl(PaisRepository paisRepository, SysSaludRepository sysSaludRepository, AuthValidator authValidator, ModelMapper modelMapper) {
         this.paisRepository = paisRepository;
-        this.userRepository = userRepository;
         this.sysSaludRepository = sysSaludRepository;
+        this.authValidator = authValidator;
         this.modelMapper = modelMapper;
     }
 
     @Override
     public ApiResponse savePais(CrearPaisDTO crearPaisDTO) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-        UserEntity user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        UserEntity user = authValidator.getCurrentUser();
         SysSaludEntity hospital = sysSaludRepository.findById(user.getHospital().getHospitalId())
                 .orElseThrow(() -> new RuntimeException("Hospital no encontrado"));
         PaisEntity pais = new PaisEntity();
--- src/main/java/com/saludSystem/infrastructure/adapters/out/persistance/repository/GenericRepository.java
+++ src/main/java/com/saludSystem/infrastructure/adapters/out/persistance/repository/GenericRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @NoRepositoryBean
