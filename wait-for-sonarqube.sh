eb4c3f45c358e4e9673b71eee8b5fb4e15a788a6
gianbdev <giancarlo.silva550@gmail.com>
2025-07-03 16:11:54 -0500

    remove | file sh

--- wait-for-sonarqube.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Esperando que SonarQube esté listo..."
-while ! curl -s http://sonarqube:9000/api/system/status | grep -q '"status":"UP"'; do
-  echo "Aún no disponible... esperando 5 segundos"
-  sleep 5
-done
-echo "✅ SonarQube está listo."
