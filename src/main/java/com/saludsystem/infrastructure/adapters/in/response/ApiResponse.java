80a75d5855a4c2857614b057d0d6b2241daa3a36
gianbdev <giancarlo.silva550@gmail.com>
2025-07-05 16:26:19 -0500

    refactor | test

--- src/main/java/com/saludsystem/infrastructure/adapters/in/response/ApiResponse.java
+++ src/main/java/com/saludsystem/infrastructure/adapters/in/response/ApiResponse.java
@@ -1,21 +1,24 @@
 package com.saludsystem.infrastructure.adapters.in.response;
 
 import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ApiResponse {
 
-    private boolean isSuccess;
+    private boolean success;
 
     @Getter
     private String message;
 
     public ApiResponse(boolean isSuccess, String message) {
-        this.isSuccess = isSuccess;
+        this.success = isSuccess;
         this.message = message;
     }
 
     public boolean getIsSuccess() {
-        return isSuccess;
+        return success;
     }
 
 }
