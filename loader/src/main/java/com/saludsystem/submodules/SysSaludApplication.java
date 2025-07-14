09e157b8d829bd7700de0108819580ddb2e15d04
gianbdev <giancarlo.silva550@gmail.com>
2025-07-13 19:39:40 -0500

    remove | comments in main class

--- loader/src/main/java/com/saludsystem/submodules/SysSaludApplication.java
+++ loader/src/main/java/com/saludsystem/submodules/SysSaludApplication.java
@@ -3,13 +3,6 @@ package com.saludsystem.submodules;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(scanBasePackages = {
-//		"com.saludsystem.infrastructure",
-//		"com.saludsystem.application",
-//		"com.saludsystem.domain"
-//})
-//@EnableJpaRepositories(basePackages = "com.saludsystem.infrastructure.adapter.jparepository") // ✅
-//@EntityScan(basePackages = "com.saludsystem.infrastructure.adapter.entity") // ✅ o donde esté UserEntity
 @SpringBootApplication
 public class SysSaludApplication {
 	public static void main(String[] args) {
