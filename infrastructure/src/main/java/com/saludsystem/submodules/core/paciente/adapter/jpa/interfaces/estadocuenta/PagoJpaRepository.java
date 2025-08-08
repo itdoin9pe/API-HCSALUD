26ff3167211222da640272b55f81bc964e12eca9
gianbdev <giancarlo.silva550@gmail.com>
2025-08-08 15:45:45 -0500

    infra | rename jpa file for estado cuenta sub core - paciente core module

--- infrastructure/src/main/java/com/saludsystem/submodules/core/paciente/adapter/jpa/interfaces/estadocuenta/PagoJpaRepository.java
+++ infrastructure/src/main/java/com/saludsystem/submodules/core/paciente/adapter/jpa/interfaces/estadocuenta/PagoJpaRepository.java
@@ -1,12 +1,12 @@
 package com.saludsystem.submodules.core.paciente.adapter.jpa.interfaces.estadocuenta;
 
-import com.saludsystem.submodules.core.paciente.adapter.entity.historialclinico.estadocuenta.PagoEntity;
+import com.saludsystem.submodules.core.paciente.adapter.entity.historialclinico.estadocuenta.EPagoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface PagoJpaRepository extends JpaRepository<PagoEntity, UUID> {
+public interface PagoJpaRepository extends JpaRepository<EPagoEntity, UUID> {
 
 }
