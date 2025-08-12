16847f138de33a60cdb9237b99fadef40fc245a5
gianbdev <giancarlo.silva550@gmail.com>
2025-08-12 11:18:05 -0500

    application | create handler add  annotation component - paciente core module

--- application/src/main/java/com/saludsystem/submodules/paciente/command/create/tratamiento/ProcedimientoCreateHandler.java
+++ application/src/main/java/com/saludsystem/submodules/paciente/command/create/tratamiento/ProcedimientoCreateHandler.java
@@ -3,7 +3,9 @@ package com.saludsystem.submodules.paciente.command.create.tratamiento;
 import com.saludsystem.submodules.paciente.mapper.tratamiento.ProcedimientoMapper;
 import com.saludsystem.submodules.paciente.model.dtos.command.create.tratamiento.ProcedimientoCreateCommand;
 import com.saludsystem.submodules.paciente.service.historia.tratamiento.procedimiento.ProcedimientoCreateService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProcedimientoCreateHandler {
 
     private final ProcedimientoCreateService procedimientoCreateService;
