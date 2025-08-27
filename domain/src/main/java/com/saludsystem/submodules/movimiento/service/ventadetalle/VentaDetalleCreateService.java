2fb82fea0428439f13b05d7debe920366f313c57
gianbdev <giancarlo.silva550@gmail.com>
2025-08-27 00:48:35 -0500

    update | create service in venta detalle

--- domain/src/main/java/com/saludsystem/submodules/movimiento/service/ventadetalle/VentaDetalleCreateService.java
+++ domain/src/main/java/com/saludsystem/submodules/movimiento/service/ventadetalle/VentaDetalleCreateService.java
@@ -1,17 +1,14 @@
 package com.saludsystem.submodules.movimiento.service.ventadetalle;
 
 import com.saludsystem.submodules.movimiento.model.VentaDetalle;
-import com.saludsystem.submodules.movimiento.port.dao.VentaDetalleDao;
 import com.saludsystem.submodules.movimiento.port.repository.VentaDetalleRepository;
 
 public class VentaDetalleCreateService {
 
     private final VentaDetalleRepository ventaDetalleRepository;
-    private final VentaDetalleDao ventaDetalleDao;
 
-    public VentaDetalleCreateService(VentaDetalleRepository ventaDetalleRepository, VentaDetalleDao ventaDetalleDao) {
+    public VentaDetalleCreateService(VentaDetalleRepository ventaDetalleRepository) {
         this.ventaDetalleRepository = ventaDetalleRepository;
-        this.ventaDetalleDao = ventaDetalleDao;
     }
 
     public VentaDetalle execute(VentaDetalle ventaDetalle) {
